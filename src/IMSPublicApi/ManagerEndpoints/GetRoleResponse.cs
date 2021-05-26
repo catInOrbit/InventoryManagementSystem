8aa4b122dc5be894937e24caa2052149bfc267de
Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
2021-05-27 00:32:50 +0700

    get roles

---
+++ src/IMSPublicApi/ManagerEndpoints/GetRoleResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryManagementSystem.PublicApi.ManagerEndpoints
+{
+    public class GetRoleResponse : BaseResponse
+    {
+        public GetRoleResponse(Guid correlationId) : base(correlationId)
+        {
+                
+        }
+
+        public GetRoleResponse()
+        {
+            
+        }
+
+        public List<string> Roles { get; set; }
+    }
+}
---
+++ src/IMSPublicApi/ManagerEndpoints/GetRoles.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using InventoryManagementSystem.PublicApi.AuthorizationEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace InventoryManagementSystem.PublicApi.ManagerEndpoints
+{
+    public class GetRoles : BaseAsyncEndpoint.WithoutRequest.WithResponse<GetRoleResponse>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IAuthorizationService _authorizationService;
+
+        public GetRoles(RoleManager<IdentityRole> roleManager, IAuthorizationService authorizationService)
+        {
+            _roleManager = roleManager;
+            _authorizationService = authorizationService;
+        }
+
+        [HttpPost("api/gerroles")]
+        [SwaggerOperation(
+            Summary = "Get all roles of system",
+            Description = "Get all roles of system",
+            OperationId = "auth.registertest",
+            Tags = new[] { "ManagerEndpoints" })
+        ]
+        public override async Task<ActionResult<GetRoleResponse>> HandleAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var isAuthorized = await _authorizationService.AuthorizeAsync(
+                HttpContext.User, "Registration",
+                UserOperations.Read);
+
+
+            if (isAuthorized.Succeeded)
+            {
+                var response = new GetRoleResponse();
+                var roleList =  _roleManager.Roles.ToList();
+
+                List<string> roles = new List<string>();
+                foreach (var role in roleList)
+                {
+                    roles.Add(role.Name);
+                }
+
+                response.Roles = roles;
+                return Ok(response);
+            }
+
+            return Unauthorized();
+
+        }
+    }
+}
