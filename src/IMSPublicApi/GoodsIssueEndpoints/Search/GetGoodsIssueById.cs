1ff619165ab89b97064f3de4ee6045944a1431d9
catInOrbit <thmmao2017@gmail.com>
2021-06-08 16:50:45 +0700

    update

---
+++ src/IMSPublicApi/GoodsIssueEndpoints/Search/GetGoodsIssueById.cs
@@ -0,0 +1,42 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using Infrastructure.Services;
+using InventoryManagementSystem.ApplicationCore.Entities.Orders;
+using InventoryManagementSystem.ApplicationCore.Interfaces;
+using InventoryManagementSystem.PublicApi.AuthorizationEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace InventoryManagementSystem.PublicApi.GoodsIssueEndpoints.Search
+{
+    public class GetGoodsIssueById : BaseAsyncEndpoint.WithRequest<GiIdRequest>.WithResponse<GiSearchResponse>
+    {
+        private IAsyncRepository<GoodsIssueOrder> _asyncRepository;
+        private readonly IAuthorizationService _authorizationService;
+
+        public GetGoodsIssueById(IAsyncRepository<GoodsIssueOrder> asyncRepository, IAuthorizationService authorizationService)
+        {
+            _asyncRepository = asyncRepository;
+            _authorizationService = authorizationService;
+        }
+        [HttpGet("api/goodsissue/{IssueId}")]
+        [SwaggerOperation(
+            Summary = "Search for good issue with all or elasticsearch field",
+            Description = "Search for good issue with all or elasticsearch field",
+            OperationId = "gi.search",
+            Tags = new[] { "GoodsIssueEndpoints" })
+        ]
+        public override async Task<ActionResult<GiSearchResponse>> HandleAsync([FromRoute] GiIdRequest request, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if(! await UserAuthorizationService.Authorize(_authorizationService, HttpContext.User, "GoodsIssue", UserOperations.Read))
+                return Unauthorized();
+
+            var response = new GiSearchResponse();
+            response.GoodsIssueOrders.Clear();
+            response.GoodsIssueOrders.Add(await _asyncRepository.GetByIdAsync(request.IssueId));
+            return Ok(response);
+        }
+    }
+}
---
+++ src/IMSPublicApi/GoodsIssueEndpoints/Search/GiIdRequest.cs
@@ -0,0 +1,7 @@
+namespace InventoryManagementSystem.PublicApi.GoodsIssueEndpoints.Search
+{
+    public class GiIdRequest
+    {
+        public string IssueId { get; set; }
+    }
+}
